2602765336e0eeda6979ddf08d36c8371c60fa5c
PascalSalesch <pascal.b.salesch@gmail.com>
2023-10-01 05:23:19 +0200

    escape backticks

--- src/parse.mjs
+++ src/parse.mjs
@@ -20,6 +20,12 @@ const ctx = await getParserContext()
  * @returns {Promise<string>} The parsed content.
  */
 export default async function parse (content) {
+  // escape backticks in the content, but not in the variables
+  content = splitByTemplateLiterals(content).reduce((content, part) => {
+    if (part.type === 'static') return content + part.value.replaceAll('`', '\\`')
+    return content + `\${${part.value}}`
+  }, '')
+
   const parse = new AsyncFunction(...Object.keys(ctx), 'return `' + content + '`')
   return await parse(...Object.values(ctx))
 }
@@ -50,4 +56,58 @@ async function getParserContext () {
     always: 'always',
     before: 'before'
   }
+}
+
+/**
+ * Seperate a string into parts of static and dynamic parts.
+ * @param {string} str - The string to split.
+ * @returns {Array<{type: "static"|"dynamic", value: string}>} - The parts of the string.
+ * @throws {Error} If the template literal is invalid.
+ */
+function splitByTemplateLiterals (str) {
+  const parts = []
+
+  let currentPart = ''
+  let isDynamic = false
+  let depth = 0
+  for (let i = 0; i < str.length; i++) {
+    const current = str.charAt(i)
+
+    if (isDynamic) {
+      if (current === '{') {
+        depth = depth + 1
+      } else if (current === '}') {
+        depth = depth - 1
+        if (depth === 0) {
+          isDynamic = false
+          parts.push({ type: 'dynamic', value: currentPart })
+          currentPart = ''
+        }
+      } else {
+        currentPart = currentPart + current
+      }
+    } else {
+      const next = str.charAt(i + 1)
+      if (current === '$' && next === '{') {
+        isDynamic = true
+        if (currentPart) {
+          parts.push({ type: 'static', value: currentPart })
+          currentPart = ''
+        }
+      } else {
+        currentPart = currentPart + current
+        if (i === str.length - 1) {
+          parts.push({ type: 'static', value: currentPart })
+          currentPart = ''
+        }
+      }
+    }
+  }
+
+  if (currentPart) {
+    if (depth > 0) throw new Error(`Invalid template literal. Missing ${depth} '}'`)
+    else throw new Error(`Invalid template literal. Unknown: ${currentPart}`)
+  }
+
+  return parts
 }
